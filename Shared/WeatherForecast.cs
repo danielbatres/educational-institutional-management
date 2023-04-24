63f96b001e60bdf0c65317f5367126b68d95291a
danielbatres <danielbatresuser@gmail.com>
2023-04-23 20:14:00 -0600

    Blazor WebAssembly hosted development template

---
+++ Shared/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace edu_institutional_management.Shared;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public string? Summary { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
