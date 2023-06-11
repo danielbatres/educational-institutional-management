78bd550a67be0323a19a33ad5d04178be93c46d3
danielbatres <danielbatresuser@gmail.com>
2023-06-11 13:33:58 -0600

    Models folder

---
+++ Server/Models/Institution.cs
@@ -0,0 +1,8 @@
+namespace edu_institutional_management;
+
+public class Institution {
+  public Guid Id { get; set; }
+  public string? Name { get; set; }
+  public string? Address { get; set; }
+  public string? PhoneNumber { get; set; }
+}
---
+++ Server/Models/User.cs
@@ -0,0 +1,9 @@
+namespace edu_institutional_management;
+
+public class User {
+  public Guid Id { get; set; }
+  public string? Name { get; set; }
+  public string? LastName { get; set; }
+  public DateTime BirthDate { get; set; }
+  public int Age { get; set; }
+}
