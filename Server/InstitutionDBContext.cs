9430a1d64de8df5a8758c6550a23c04cc94414ad
Daniel Batres <danielbatresuser@gmail.com>
2023-06-13 10:28:14 -0600

    Add some changes

--- Server/InstitutionDBContext.cs
+++ Server/InstitutionDBContext.cs
@@ -1,12 +1,44 @@
-using Microsoft.EntityFramework.Algo;
+using edu_institutional_management.Server.Models;
+using Microsoft.EntityFrameworkCore;
 
-public class InstitutionDBContext : DBContext {
-    public DBSet<User> Users { get; set; }
-    public DBSet<Institution> Institutions { get; set; }
+namespace edu_institutional_management.Server;
 
-    public InstitutionDBContext(DBOptions options) : base(options) {}
+public class InstitutionDBContext : DbContext {
+    public DbSet<User> Users { get; set; }
+    public DbSet<Institution> Institutions { get; set; }
+
+    public InstitutionDBContext(DbContextOptions<InstitutionDBContext> options) : base(options) {}
 
     protected override void OnModelCreating(ModelBuilder modelBuilder) {
-        modelBuilder.Entity();
+        modelBuilder.Entity<Institution>(institution => {
+            institution.ToTable("Institution");
+            institution.HasKey(x => x.Id);
+            institution.Property(x => x.Name);
+            institution.Property(x => x.Address);
+            institution.Property(x => x.PhoneNumber);
+        });
+
+        modelBuilder.Entity<User>(user => {
+            user.ToTable("User");
+            user.HasKey(x => x.Id);
+            user.Property(x => x.Name);
+            user.Property(x => x.LastName);
+            user.Property(x => x.BirthDate);
+        });
+
+        modelBuilder.Entity<Register>(register => {
+            register.ToTable("Register");
+            register.HasKey(x => x.Id);
+            register.Property(x => x.Email);
+            register.Property(x => x.Password);
+            register.Property(x => x.AuthenticationMethod);
+        });
+
+        modelBuilder.Entity<OnlineStatus>(onlineStatus => {
+            onlineStatus.ToTable("OnlineStatus");
+            onlineStatus.HasKey(x => x.Id);
+            onlineStatus.Property(x => x.LastConnection);
+            onlineStatus.Property(x => x.Status);
+        });
     }
 }
--- Server/Models/Permission.cs
+++ Server/Models/Permission.cs
@@ -4,4 +4,5 @@ public class Permission {
   public Guid Id { get; set; }
   public string? Name { get; set; }
   public string? Description { get; set; }
+  public Role Role { get; set; }
 }
