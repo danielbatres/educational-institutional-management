5d0a6d5806b4c758ce088db7df14cd03ba193a50
danielbatres <danielbatresuser@gmail.com>
2023-09-20 07:50:26 -0600

    Provisional changes

--- Shared/Models/Event.cs
+++ Shared/Models/Event.cs
@@ -1,6 +1,7 @@
 namespace edu_institutional_management.Shared.Models;
 
 public class Event : MainDescription {
+  public Guid Id { get; set; }
   public int ParticipantsCount { get; set; }
   public DateTime Date { get; set; }
 }
